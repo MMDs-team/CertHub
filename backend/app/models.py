2ce85c85353d49e045bb2507e3cad3ee75a6f53a
Khandan <taowqurdu13@gmail.com>
2024-12-30 02:18:27 +0330

    Template model have been added to models.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Template(models.Model):
+    template_id = models.AutoField(primary_key=True, editable=False)
+
+    image = models.ImageField(upload_to="templates/images", blank=True)
+    file = models.FileField(upload_to="templates/files")
+
+    is_public = models.BooleanField(default=False, blank=True)
+    usage = models.PositiveIntegerField(default=0, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='template_owner', null=True, blank=True)
+
+    is_active = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return f"Template {self.template_id} - {self.file.name}"
