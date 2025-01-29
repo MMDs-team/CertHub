693a1d42db9d1b0db28202d82d8f5060453f78b2
mmd <mohmad1382m@gmail.com>
2025-01-29 18:03:19 +0330

    update user serializer with user profile

--- backend/app/serializers/user_serializers.py
+++ backend/app/serializers/user_serializers.py
@@ -5,21 +5,46 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     organization = serializers.SerializerMethodField(read_only=True)
+
     isAdmin = serializers.SerializerMethodField(read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
+    email = serializers.SerializerMethodField(read_only=True)
+    first_name = serializers.SerializerMethodField(read_only=True)
+    last_name = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = UserProfile
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'organization', 'isAdmin']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'isAdmin', 'organization']
 
     def get_organization(self, obj):
         return obj.profile.organization if hasattr(obj, 'profile') else None
 
     def get_isAdmin(self, obj):
-        return obj.is_staff
+        return obj.user.is_staff
+
+    def get_username(self, obj):
+        return obj.user.username 
+
+    def get_email(self, obj):
+        return obj.user.email 
+
+    def get_first_name(self, obj):
+        return obj.user.first_name 
+
+    def get_last_name(self, obj):
+        return obj.user.last_name 
+
 
 class UserSerializerWithToken(UserSerializer):
     organization = serializers.SerializerMethodField(read_only=True)
-    isAdmin = serializers.SerializerMethodField(read_only=True)
     token = serializers.SerializerMethodField(read_only=True)
+
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
+    email = serializers.SerializerMethodField(read_only=True)
+    first_name = serializers.SerializerMethodField(read_only=True)
+    last_name = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = UserProfile
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'organization', 'isAdmin', 'token']
@@ -28,8 +53,20 @@ class UserSerializerWithToken(UserSerializer):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
 
+    def get_organization(self, obj):
+        return obj.profile.organization if hasattr(obj, 'profile') else None
+
     def get_isAdmin(self, obj):
         return obj.is_staff
 
-    def get_organization(self, obj):
-        return obj.profile.organization if hasattr(obj, 'profile') else None
+    def get_username(self, obj):
+        return obj.user.username 
+
+    def get_email(self, obj):
+        return obj.user.email 
+
+    def get_first_name(self, obj):
+        return obj.user.first_name 
+
+    def get_last_name(self, obj):
+        return obj.user.last_name 
