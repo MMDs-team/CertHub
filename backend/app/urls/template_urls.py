031d457779b2f4ec92964d83d3ef79058d4fe83c
mmd <mohmad1382m@gmail.com>
2025-02-19 12:50:06 +0330

    add template history and paging to public templates

--- backend/app/urls/template_urls.py
+++ backend/app/urls/template_urls.py
@@ -4,7 +4,8 @@ from app.views import template_views as views
 urlpatterns = [
     path('', views.get_public_templates, name='get_public_templates'),
     path('<int:pk>', views.get_template, name='get_template'),
-    path('create/', views.create_template, name='create_template'),
+    path('history', views.template_history, name='template_history'),
+    path('create', views.create_template, name='create_template'),
     path('remove/<int:pk>', views.remove_template, name='remove_template'),
     path('count_fields/<int:pk>', views.count_blank_fields, name='count_blank_fiels'),
     path('make_doc/<int:pk>', views.make_doc, name='make_doc'),
